0256868f75e3525c6eb406af92075f3685c62256
therakeshhr <therakeshhr@gmail.com>
2026-07-01 12:06:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+const counter = document.getElementById("counter");
+
+document.getElementById("increase").onclick = () => {
+    count++;
+    counter.textContent = count;
+};
+
+document.getElementById("decrease").onclick = () => {
+    count--;
+    counter.textContent = count;
+};
+
+document.getElementById("reset").onclick = () => {
+    count = 0;
+    counter.textContent = count;
+};
